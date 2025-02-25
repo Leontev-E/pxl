3d79f21c498a405fa5ed9e705e3133df7e8d6f32
Leontev-E <110973495+Leontev-E@users.noreply.github.com>
2025-02-25 16:06:56 +0400

    Add files via upload

---
+++ successPxl.js
@@ -0,0 +1,46 @@
+(function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const pxl = urlParams.get('pxl');
+    const subid = '{subid}';
+    const contentIds = urlParams.get('content_ids');
+
+    if (pxl) {
+        sessionStorage.setItem('pxl', pxl);
+    }
+
+    if (subid) {
+        sessionStorage.setItem('external_id', subid);
+        sessionStorage.setItem('event_id', subid);
+    }
+
+    if (contentIds) {
+        sessionStorage.setItem('content_ids', contentIds);
+    }
+
+    if (pxl) {
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+        fbq('init', pxl);
+
+        if (contentIds) {
+            fbq('track', 'PageView', { content_ids: contentIds });
+        } else {
+            fbq('track', 'PageView');
+        }
+    }
+})();
