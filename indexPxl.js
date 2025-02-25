443d7bea7a6bb16ccdf990ba4747b9acc775198b
Leontev-E <110973495+Leontev-E@users.noreply.github.com>
2025-02-25 16:55:34 +0400

    Update indexPxl.js

--- indexPxl.js
+++ indexPxl.js
@@ -1,14 +1,21 @@
 (function () {
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+
     const urlParams = new URLSearchParams(window.location.search);
     const pxl = urlParams.get('pxl');
-    const subid = '{subid}';
     const contentIds = urlParams.get('content_ids');
+
+    const subid = getCookie('_subid');
 
     if (pxl) {
         sessionStorage.setItem('pxl', pxl);
     }
 
-    if (subid && subid !== '{subid}') {
+    if (subid) {
         sessionStorage.setItem('external_id', subid);
         sessionStorage.setItem('event_id', subid);
     }
@@ -21,7 +28,7 @@
         !function (f, b, e, v, n, t, s) {
             if (f.fbq) return;
             n = f.fbq = function () {
-                n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+                n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
             };
             if (!f._fbq) f._fbq = n;
             n.push = n;
@@ -32,7 +39,7 @@
             t.async = !0;
             t.src = v;
             s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
+            s.parentNode.insertBefore(t, s);
         }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
 
         fbq('init', pxl);
