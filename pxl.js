825f1a67f622aad807b31f2b1694f272d82caba8
Leontev-E <110973495+Leontev-E@users.noreply.github.com>
2025-03-04 21:22:13 +0400

    Update pxl.js

--- pxl.js
+++ pxl.js
@@ -1,22 +1,22 @@
-(function () {
-    const pxl = sessionStorage.getItem('pxl');
-    const subid = sessionStorage.getItem('external_id');
-    const contentIds = sessionStorage.getItem('content_ids');
+document.addEventListener("DOMContentLoaded", function() {
+  const pxl = sessionStorage.getItem('pxl');
+  const subid = sessionStorage.getItem('external_id');
+  const contentIds = sessionStorage.getItem('content_ids');
 
-    if (pxl) {
-        let url = `https://www.facebook.com/tr?id=${pxl}&ev=Lead&noscript=1`;
-        if (subid) {
-            url += `&external_id=${subid}&event_id=${subid}`;
-        }
-        if (contentIds) {
-            url += `&content_ids=${encodeURIComponent(contentIds)}`;
-        }
-        const img = new Image(1, 1);
-        img.style.display = 'none';
-        img.src = url;
-        document.body.appendChild(img);
+  if (pxl) {
+    let url = `https://www.facebook.com/tr?id=${pxl}&ev=Lead&noscript=1`;
+    if (subid) {
+      url += `&external_id=${subid}&event_id=${subid}`;
     }
-})();
+    if (contentIds) {
+      url += `&content_ids=${encodeURIComponent(contentIds)}`;
+    }
+    const img = new Image(1, 1);
+    img.style.display = 'none';
+    img.src = url;
+    document.body.appendChild(img);
+  }
+});
 
 (function() {
   function getCookie(name) {
