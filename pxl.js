c0b09faf3587ee9888ca6039e68bd9825b34bd69
Leontev-E <110973495+Leontev-E@users.noreply.github.com>
2025-03-05 09:36:12 +0400

    Update pxl.js
    add google tag

--- pxl.js
+++ pxl.js
@@ -1,3 +1,4 @@
+// fb pxl
 document.addEventListener("DOMContentLoaded", function() {
   const pxl = sessionStorage.getItem('pxl');
   const subid = sessionStorage.getItem('external_id');
@@ -18,24 +19,33 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
+// google tag
 (function() {
   function getCookie(name) {
     const value = "; " + document.cookie;
     const parts = value.split("; " + name + "=");
     if (parts.length === 2) return parts.pop().split(";").shift();
   }
+
   var gt = getCookie("gt") || "";
   var pt = getCookie("pt") || "";
   var ad_id = getCookie("ad_id") || "";
   var acc = getCookie("acc") || "";
   var buyer = getCookie("buyer") || "";
+
   function updateURL() {
     var currentURL = window.location.href;
     var hasParams = currentURL.includes("?");
-    var updatedURL = currentURL + (hasParams ? "&" : "?") + "gt=" + encodeURIComponent(gt) + "&pt=" + encodeURIComponent(pt) + "&ad_id=" + encodeURIComponent(ad_id) + "&acc=" + encodeURIComponent(acc) + "&buyer=" + encodeURIComponent(buyer);
+    var updatedURL = currentURL + (hasParams ? "&" : "?") +
+      "gt=" + encodeURIComponent(gt) +
+      "&pt=" + encodeURIComponent(pt) +
+      "&ad_id=" + encodeURIComponent(ad_id) +
+      "&acc=" + encodeURIComponent(acc) +
+      "&buyer=" + encodeURIComponent(buyer);
     window.history.replaceState({ path: updatedURL }, "", updatedURL);
     loadGTM(gt, pt);
   }
+
   function loadGTM(gt, pt) {
     var gtmScript = document.createElement("script");
     gtmScript.src = "https://www.googletagmanager.com/gtag/js?id=" + gt;
@@ -49,5 +59,8 @@ document.addEventListener("DOMContentLoaded", function() {
       gtag("event", "conversion", { "send_to": gt + "/" + pt });
     };
   }
-  updateURL();
+
+  if (gt || pt) {
+    updateURL();
+  }
 })();
