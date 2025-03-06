ffa1a0fd7fbdb6c50e2d9f5cbd8cac3697bd0b6d
Leontev-E <110973495+Leontev-E@users.noreply.github.com>
2025-03-06 09:40:19 +0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,74 @@
+// Facebook Pixel
+document.addEventListener("DOMContentLoaded", () => {
+  const pxl = sessionStorage.getItem('pxl');
+  if (!pxl) return;
+
+  const subid = sessionStorage.getItem('external_id');
+  const contentIds = sessionStorage.getItem('content_ids');
+
+  // Используем объект URL для сборки запроса
+  const fbUrl = new URL("https://www.facebook.com/tr");
+  fbUrl.searchParams.set("id", pxl);
+  fbUrl.searchParams.set("ev", "Lead");
+  fbUrl.searchParams.set("noscript", "1");
+
+  if (subid) {
+    fbUrl.searchParams.set("external_id", subid);
+    fbUrl.searchParams.set("event_id", subid);
+  }
+  if (contentIds) {
+    fbUrl.searchParams.set("content_ids", contentIds);
+  }
+
+  const img = new Image(1, 1);
+  img.style.display = "none";
+  img.src = fbUrl.toString();
+  document.body.appendChild(img);
+});
+
+// Google Tag
+(() => {
+  // Функция для получения значения cookie
+  const getCookie = name => {
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    return match ? match[2] : "";
+  };
+
+  // Получаем необходимые cookie
+  const gt = getCookie("gt");
+  const pt = getCookie("pt");
+  const ad_id = getCookie("ad_id");
+  const acc = getCookie("acc");
+  const buyer = getCookie("buyer");
+
+  // Функция для обновления URL с добавлением UTM-параметров
+  const updateURL = () => {
+    const url = new URL(window.location.href);
+    url.searchParams.set("gt", gt);
+    url.searchParams.set("pt", pt);
+    url.searchParams.set("ad_id", ad_id);
+    url.searchParams.set("acc", acc);
+    url.searchParams.set("buyer", buyer);
+    window.history.replaceState({ path: url.toString() }, "", url.toString());
+    loadGTM(gt, pt);
+  };
+
+  // Функция для загрузки и инициализации Google Tag Manager
+  const loadGTM = (gt, pt) => {
+    const gtmScript = document.createElement("script");
+    gtmScript.src = `https://www.googletagmanager.com/gtag/js?id=${gt}`;
+    gtmScript.async = true;
+    document.head.appendChild(gtmScript);
+    gtmScript.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      const gtag = (...args) => dataLayer.push(args);
+      gtag("js", new Date());
+      gtag("config", gt);
+      gtag("event", "conversion", { send_to: `${gt}/${pt}` });
+    };
+  };
+
+  if (gt || pt) {
+    updateURL();
+  }
+})();
