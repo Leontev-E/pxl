8ee34bffaa2a37c1a29cae3c565ed9fe0925a701
Leontev-E <110973495+Leontev-E@users.noreply.github.com>
2025-03-25 14:45:29 +0400

    Update indexPxl.js

--- indexPxl.js
+++ indexPxl.js
@@ -55,27 +55,45 @@
     }
 
     // Обработка переходов для domonetka
-    if (typeof domonetka !== 'undefined' && domonetka && domonetka.trim() !== '' && domonetka !== '{domonetka}') {
-        try {
-            window.onpopstate = function (event) {
-                const currentUrlParams = new URLSearchParams(window.location.search);
-                const newUrlParams = new URLSearchParams();
-                for (const [key, value] of currentUrlParams.entries()) {
-                    if (key !== 'ad') {
-                        newUrlParams.append(key, value);
-                    }
-                }
-                const newUrl = `${domonetka}?${newUrlParams.toString()}`;
-                location.replace(newUrl);
-            };
+if (typeof domonetka !== 'undefined' && domonetka && domonetka.trim() !== '' && domonetka !== '{domonetka}') {
+    try {
+        window.onpopstate = function (event) {
+            const currentUrlParams = new URLSearchParams(window.location.search);
+            const newUrlParams = new URLSearchParams();
 
-            for (let i = 0; i < 10; i++) {
-                setTimeout(() => history.pushState({}, "", window.location.href), i * 50);
+            if (currentUrlParams.has('source')) {
+                newUrlParams.set('source', currentUrlParams.get('source'));
             }
-        } catch (error) {
-            console.error(error);
+            if (currentUrlParams.has('ev')) {
+                newUrlParams.set('ev', currentUrlParams.get('ev'));
+            }
+            if (currentUrlParams.has('acc')) {
+                newUrlParams.set('sub_id_2', currentUrlParams.get('acc'));
+            }
+            if (currentUrlParams.has('placement')) {
+                newUrlParams.set('sub_id_3', currentUrlParams.get('placement'));
+            }
+            if (currentUrlParams.has('buyer')) {
+                newUrlParams.set('sub_id_4', currentUrlParams.get('buyer'));
+            }
+            if (currentUrlParams.has('pxl')) {
+                newUrlParams.set('sub_id_8', currentUrlParams.get('pxl'));
+            }
+            if (currentUrlParams.has('adset')) {
+                newUrlParams.set('sub_id_5', currentUrlParams.get('adset'));
+            }
+
+            const newUrl = `${domonetka}?${newUrlParams.toString()}`;
+            location.replace(newUrl);
+        };
+
+        for (let i = 0; i < 10; i++) {
+            setTimeout(() => history.pushState({}, "", window.location.href), i * 50);
         }
+    } catch (error) {
+        console.error(error);
     }
+}
 
     // Установка UTMCookies и интеграция Google Tag Manager
     function setUTMCookies() {
