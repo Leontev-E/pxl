2dd222320ccab8772cfe9d5dbf9884fb9a2fe10e
Евгений Леонтьев <110973495+Leontev-E@users.noreply.github.com>
2026-01-30 16:05:17 +0400

    Update indexPxl.js

--- indexPxl.js
+++ indexPxl.js
@@ -124,6 +124,58 @@
         }
     }
 
+    // Init TikTok Pixel PageView when tt pixel present.
+(function initTikTokPageView() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const pixelId = (urlParams.get('pixel') || sessionStorage.getItem('tt_pixel') || '').trim();
+    if (!pixelId) return;
+
+    try {
+        const key = 'tt_pageview_sent_' + pixelId;
+        if (sessionStorage.getItem(key) === '1') return;
+        sessionStorage.setItem(key, '1');
+    } catch (e) { }
+
+    // грузим events.js только один раз
+    if (!window.ttq) {
+        !function (w, d, t) {
+            w.TiktokAnalyticsObject = t;
+            var ttq = w[t] = w[t] || [];
+            ttq.methods = ["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];
+            ttq.setAndDefer = function (t, e) {
+                t[e] = function () { t.push([e].concat([].slice.call(arguments, 0))); };
+            };
+            for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]);
+            ttq.instance = function (t) {
+                for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++) ttq.setAndDefer(e, ttq.methods[n]);
+                return e;
+            };
+            ttq.load = function (e, n) {
+                var i = "https://analytics.tiktok.com/i18n/pixel/events.js";
+                ttq._i = ttq._i || {};
+                ttq._i[e] = [];
+                ttq._i[e]._u = i;
+                ttq._t = ttq._t || {};
+                ttq._t[e] = +new Date;
+                ttq._o = ttq._o || {};
+                ttq._o[e] = n || {};
+                var o = d.createElement("script");
+                o.type = "text/javascript";
+                o.async = !0;
+                o.src = i + "?sdkid=" + e + "&lib=" + t;
+                var a = d.getElementsByTagName("script")[0];
+                a.parentNode.insertBefore(o, a);
+            };
+        }(window, document, 'ttq');
+    }
+
+    try {
+        window.ttq.load(pixelId);
+        window.ttq.page();
+    } catch (e) { }
+})();
+
+
     // Handle back/forward for domonetka redirects.
     if (domonetkaActive) {
         try {
@@ -315,4 +367,5 @@
         // ignore
     }
 })();
+
 
